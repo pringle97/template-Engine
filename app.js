4b474d6993123c95ad2cd5df390d9b5eefd5e4df
pringle97 <malia.pringle@gmail.com>
2021-12-11 18:27:37 -0800

    cleaning code

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const fse = require('fs-extra');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
